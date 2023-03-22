0c20b2af291db08b6953d6c3b265fe505bd8c2d4
iliedavid1 <david.ilie@stud.acs.upb.ro>
2023-03-22 16:28:36 +0200

    proiect

---
+++ backend/src/main/java/com/store/backend/BackendApplication.java
@@ -0,0 +1,14 @@
+package com.store.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		System.out.println("ceva");
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
