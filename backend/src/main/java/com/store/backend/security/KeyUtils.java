ea97d43923e1e229fc918b0455cdad0fccdc655b
iliedavid1 <david.ilie@stud.acs.upb.ro>
2023-04-25 22:55:05 +0300

    ON-25 Login/Register Back

---
+++ backend/src/main/java/com/store/backend/security/KeyUtils.java
@@ -0,0 +1,89 @@
+package com.store.backend.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.security.*;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.EncodedKeySpec;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Objects;
+
+@Component
+public class KeyUtils {
+    @Value("access-token.private")
+    private String accessTokenPrivateKeyPath;
+
+    @Value("access-token.public")
+    private String accessTokenPublicKeyPath;
+
+    @Value("refresh-token.private")
+    private String refreshTokenPrivateKeyPath;
+
+    @Value("refresh-token.public")
+    private String refreshTokenPublicKeyPath;
+
+    private KeyPair _accessTokenKeyPair;
+    private KeyPair _refreshTokenKeyPair;
+
+    private KeyPair getAccessTokenKeyPair() {
+        if (Objects.isNull(_accessTokenKeyPair)) {
+            _accessTokenKeyPair = getKeyPair(accessTokenPublicKeyPath, accessTokenPrivateKeyPath);
+        }
+        return _accessTokenKeyPair;
+    }
+
+    private KeyPair getRefreshTokenKeyPair() {
+        if (Objects.isNull(_refreshTokenKeyPair)) {
+            _refreshTokenKeyPair = getKeyPair(refreshTokenPublicKeyPath, refreshTokenPrivateKeyPath);
+        }
+        return _refreshTokenKeyPair;
+    }
+
+    private KeyPair getKeyPair(String publicKeyPath, String privateKeyPath) {
+        KeyPair keyPair;
+
+        File publicKeyFile = new File(publicKeyPath);
+        File privateKeyFile = new File(privateKeyPath);
+
+        if (publicKeyFile.exists() && privateKeyFile.exists()) {
+            try {
+                KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+
+                byte[] publicKeyBytes = Files.readAllBytes(publicKeyFile.toPath());
+                EncodedKeySpec publicKeySpec = new X509EncodedKeySpec(publicKeyBytes);
+                PublicKey publicKey = keyFactory.generatePublic(publicKeySpec);
+
+                byte[] privateKeyBytes = Files.readAllBytes(privateKeyFile.toPath());
+                PKCS8EncodedKeySpec privateKeySpec = new PKCS8EncodedKeySpec(privateKeyBytes);
+                PrivateKey privateKey = keyFactory.generatePrivate(privateKeySpec);
+
+                keyPair = new KeyPair(publicKey, privateKey);
+                return keyPair;
+            } catch (NoSuchAlgorithmException | InvalidKeySpecException | IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            throw new RuntimeException("Key pair not found");
+        }
+    }
+
+    public RSAPublicKey getAccessTokenPublicKey() {
+        return (RSAPublicKey) getAccessTokenKeyPair().getPublic();
+    }
+    public RSAPrivateKey getAccessTokenPrivateKey() {
+        return (RSAPrivateKey) getAccessTokenKeyPair().getPrivate();
+    }
+    public RSAPublicKey getRefreshTokenPublicKey() {
+        return (RSAPublicKey) getRefreshTokenKeyPair().getPublic();
+    }
+    public RSAPrivateKey getRefreshTokenPrivateKey() {
+        return (RSAPrivateKey) getRefreshTokenKeyPair().getPrivate();
+    }
+}
